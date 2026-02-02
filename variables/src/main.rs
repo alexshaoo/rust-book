6811aed12b137ce4ee8073c5f8754503fb433a71
Alex Shao <alexshao90@gmail.com>
2026-02-02 15:04:30 -0500

    variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    println!("{0}, {1}", tup.0, tup.2);
+    let (x, y, z) = tup;
+    println!("{x}, {y}");
+    let mut mutable: (i32, i32) = (1, 2);
+    mutable.0 += 1;
+    mutable.1 += 100;
+    println!("{0}, {1}", mutable.0, mutable.1);
+    let months: [&str; 12] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; // don't need this type, rust can figure this out, just did it for transparency
+    let a = [3; 5]; // [3, 3, 3, 3, 3];
+    let message = "The temperature today is:";
+//    let xx = [message, 100];
+//    println!("{} {}", x[0], x[1]);
+    let t = ([1; 2], [3; 4]);
+    let (a, b) = t;
+    println!("{}", a[0] + t.1[0]);
 }
